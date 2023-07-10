060e16393ac62f3681b4d7085be65e47cf781ef1
Leobritt <leobritto160@gmail.com>
2023-07-10 11:20:35 -0300

    change from InheritedWidget to StatefulWidget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,62 +1,83 @@
-import 'package:first_flutter_project/home_controller.dart';
 import 'package:flutter/material.dart';
-import 'home_controller.dart';
 
-//boa praticar colocar o nome da class ...page quando for um widget q ocupa a tela inteira
-class HomePage extends StatelessWidget {
-  //construtor const e add uma key
-  //key é um id da arvore widget n é obrigatorio passar
+/*boa praticar colocar o nome da class ...page quando for um widget q ocupa a tela inteira*/
+
+//Quando For StatefulWidget criar outra class privada isso é um principio do solid
+//chamado singleResponsability
+class HomePage extends StatefulWidget {
+  //construtor const e add uma key é uma id da arvore widget n é obrigatorio passar
   const HomePage({super.key});
 
   @override
-  //build context vai recuparar/acessar as infos do home_controller pai da home_page
-  //context é da app inteira e eh oq tem construido ate o momento
-  Widget build(BuildContext context) {
-    //é possivel pega outras infos usando o
-    var controller = HomeController.of(context);
-    final size = MediaQuery.of(context).size;
-    print(size);
-    final navigation = Navigator.of(context);
-    print(navigation);
-    // ! avisando q pode ser null
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  var counterAdd = 0;
+
+  /*chamado quando o widget for construido
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  //chamado quando o widget for destruido
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+  */
+
+  /*build context vai recuparar/acessar as infos do home_controller pai da home_page
+  context é da app inteira e eh oq tem construido ate o momento
+  */
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text("Contador de cliques"),
-          backgroundColor: Color.fromARGB(255, 32, 31, 30)),
-      backgroundColor: Color.fromARGB(255, 117, 117, 117),
-      floatingActionButton:
-          Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-        FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            controller.increment();
-            // ignore_for_file: avoid_print
-            print("clicou no botão de soma");
-          },
-        ),
+        title: const Text("Contador de cliques"),
+        backgroundColor: Color.fromARGB(134, 61, 187, 255),
+      ),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () {
+              if (counterAdd == 0) {
+              } else {
+                setState(() {
+                  counterAdd--;
+                });
+              }
+            },
+          ),
 
-        const SizedBox(height: 16), // Espaçamento entre os botões
+          //const SizedBox(width: 20), Espaçamento entre os botões
 
-        FloatingActionButton(
-          child: const Icon(Icons.remove),
-          onPressed: () {
-            // ignore_for_file: avoid_print
-            controller.decrement();
-            print("clicou no botão de subtração");
-          },
-        ),
-
-        FloatingActionButton(
-          child: const Icon(Icons.circle),
-          onPressed: () {
-            // ignore_for_file: avoid_print
-            controller.reset();
-            print("clicou no botão de subtração");
-          },
-        ),
-      ]),
-      body: Center(child: Text('Quantidade de Cliques ${controller.cont}')),
+          FloatingActionButton(
+            child: const Icon(Icons.circle),
+            onPressed: () {
+              setState(() {
+                counterAdd = 0;
+              });
+            },
+          ),
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                counterAdd++;
+              });
+            },
+          ),
+        ],
+      ),
+      body: Center(child: Text('Quantidade de Cliques ${counterAdd}')),
     );
   }
 }
